ea0d904ae6e61face8851241f7fa1b963a458cc4
Phillip <phillip.meisinger@gmx.de>
2025-10-03 15:33:31 +0200

    external interupt for PC1 added

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,33 @@
 #include "avr/io.h"
+#include "avr/interrupt.h"
 #include "util/delay.h"
 
 #include <stdlib.h>
 
 #include "screen.h"
 
+
+int8_t digit = 1;
+uint8_t up = 1;
+
+
 int main ()
 {
     init_screen();
 
     uint8_t number = 1;
-    int8_t digit = 1;
-    uint8_t up = 1;
+
+    // enable interrups that are in PCMSK1 register (PORTC) 
+    PCICR |= (1 << PCIE1);
+
+    // enable the interupt PCINT9 (that is a gpio function of PC1 )
+    PCMSK1 |= (1 << PCINT9);
+
+    sei();
+
     while(1)
     {
         write_screen( bcd_to_7_seg[number], (1<<digit) );
-
-        if ( digit <= 0 )
-        {
-            up = 1;
-        }
-        else if ( digit >= 3 )
-            up = 0;
-
-        if ( up )
-            digit++;
-        else
-            digit--;
 
         number = (number+1)%10;
         
@@ -35,6 +36,25 @@ int main ()
 }
 
 
+// this is executed on all external interrupts from PORTC
+ISR(PCINT1_vect)
+{
+    if ( PINC & (1<<PINC1) )
+        return;
+
+    if ( digit <= 0 )
+    {
+        up = 1;
+    }
+    else if ( digit >= 3 )
+        up = 0;
+
+    if ( up )
+        digit++;
+    else
+        digit--;
+
+}
 
 
 
