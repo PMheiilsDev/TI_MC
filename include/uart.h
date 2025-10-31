9ed2a89439712db423bb9081c301cf114f4a39f8
Phillip <phillip.meisinger@gmx.de>
2025-10-31 22:14:04 +0100

    errors fixed

--- include/uart.h
+++ include/uart.h
@@ -4,16 +4,15 @@
 #include "avr/io.h"
 
 
-#define CPU_FREQ 16000000
+#define CPU_FREQ 16000000ul
 
 #ifndef BAUD_RATE
-    #define BAUD_RATE 9600
+    #define BAUD_RATE 9600ul
 #endif
 
 /// @brief initialise harware uart 0 for tx and rx 
 void uart0_init()
 {
-    // for this functionality all bits 0  
     UCSR0A = 0;
 
     UCSR0B = 0;
@@ -23,20 +22,23 @@ void uart0_init()
     UCSR0C = 0;
 
     UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01);
-    
+
+    // set prescaler for baud rate  
+    // UART_FREQ = CPU_FREQ/16 if U2Xn is 0
+    UBRR0 = CPU_FREQ/(16*BAUD_RATE) - 1;
+
 }
 
-void uart0_write_bytes_blocking( uint8_t* ptr, uint32_t len )
+void uart0_write_bytes_blocking( char* ptr, uint32_t len )
 {
     for ( uint32_t i = 0; i < len; i++ )
     {
-        while( UCSR0A & (1<<UDRE0) );
+        while( !(UCSR0A & (1<<UDRE0)) );
         UDR0 = *( ptr + i );
-        i++; 
     }
-    
+
     // last byte is still been shifted out here
-    while( UCSR0A & (1<<UDRE0) );
+    while( !(UCSR0A & (1<<UDRE0)) );
 }
 
 #endif
